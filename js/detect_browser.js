98a138239b64e18ac621c4f0171fa799dd3a39bd
Rajdeep <rajdeep.sarkar@uwe.ac.uk>
2025-10-26 09:46:51 +0530

    Initial commit: Portfolio with HTML, CSS, JS, images, and videos

---
+++ js/detect_browser.js
@@ -0,0 +1,7 @@
+
+
+if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
+{
+  
+  window.open('unsupported.html','_top');
+}
